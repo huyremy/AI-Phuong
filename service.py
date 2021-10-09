790701fb729f3eeacf18e0224410a68e0b18424d
Nguyễn Quang Huy <huynq@isi.com.vn>
2021-10-09 18:45:34 +0700

    Update service.py

--- service.py
+++ service.py
@@ -14,6 +14,8 @@ engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 #print(voices[1].id)
 engine.setProperty('voice',voices[0].id)
+#engine.setProperty('voice',voices[1].id)
+
 
 def speak(audio):
     engine.say(audio)
